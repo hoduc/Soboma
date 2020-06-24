750811962a57366c1b590570308ddf6b4a69cc5e
hoduc <duc_ho90@yahoo.com>
2020-06-24 00:45:01 -0700

    Fake UserAgent to get the right content for twitter

--- soboma.py
+++ soboma.py
@@ -1,6 +1,9 @@
 import requests
 import configparser
+from fake_useragent import UserAgent
 
+UA = UserAgent()
+HEADER = {'User-Agent' : str(UA.chrome) }
 CONFIG_FILE_NAME = "config"
 TWITTER_CONFIG_SECTION = "twitter"
 SEARCH_URL_KEY = "search_url"
@@ -10,7 +13,7 @@ twitter_config.read(CONFIG_FILE_NAME)
 search_url = twitter_config[TWITTER_CONFIG_SECTION][SEARCH_URL_KEY]
 babylon_search_url = search_url + "babylon"
 
-r = requests.get(babylon_search_url)
+r = requests.get(babylon_search_url, headers=HEADER)
 if r.status_code == 200:    
     print(r.json())
 else:
