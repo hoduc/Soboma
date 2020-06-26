33d788d1811cb67fb04be49649873c7fe1e76c07
hoduc <duc_ho90@yahoo.com>
2020-06-26 00:17:13 -0700

    Filter out no timestamp or wrong twitter post. Add simple text UI with PyQt

--- soboma.py
+++ soboma.py
@@ -1,7 +1,9 @@
 import requests
 import configparser
+import datetime
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
 
 
 UA = UserAgent()
@@ -10,6 +12,7 @@ CONFIG_FILE_NAME = "config"
 TWITTER_CONFIG_SECTION = "twitter"
 SEARCH_URL_KEY = "search_url"
 TWITTER_IDS_KEY = "twitter_ids"
+UI_KEY = "ui"
 DEBUG_KEY = "debug"
 DEBUG_PRINT_KEY = "debug_print"
 
@@ -17,6 +20,7 @@ twitter_config = configparser.ConfigParser()
 twitter_config.read(CONFIG_FILE_NAME)
 search_url = twitter_config[TWITTER_CONFIG_SECTION][SEARCH_URL_KEY]
 twitter_ids = twitter_config[TWITTER_CONFIG_SECTION][TWITTER_IDS_KEY].split(",")
+ui = twitter_config[TWITTER_CONFIG_SECTION].getboolean(UI_KEY)
 debug = twitter_config[TWITTER_CONFIG_SECTION].getboolean(DEBUG_KEY)
 debug_print = twitter_config[TWITTER_CONFIG_SECTION].getboolean(DEBUG_PRINT_KEY)
 
@@ -28,6 +32,7 @@ def dbgp(msg):
     dbg_print(msg, debug_print)
 
 if __name__ == "__main__":
+    dtos = {}
     for twitter_id in twitter_ids:
         dbgp(('url=', search_url + twitter_id))
         r = requests.get(search_url + twitter_id, headers=HEADER)
@@ -38,7 +43,7 @@ if __name__ == "__main__":
             profile_li = soup.find("li", class_ = "AdaptiveStreamUserGallery")
             profile_img = profile_li.find("img", class_ = "ProfileCard-avatarImage")["src"]
             #[tweets, followings, followers]
-            profile_stats = ["".join(elem.get_text().split()) for elem in profile_li.find_all("span", class_ = "ProfileCardStats-statValue")]
+            profile_stats = ["".join(elem.get_text().split()) for elem in profile_li.find_all("span", class_ = "ProfileCardStats-stat")]
             # TODO: is there an elvis operator in python?
             profile_bio = profile_li.find("p", class_ = "ProfileCard-bio")
             if profile_bio:
@@ -54,7 +59,7 @@ if __name__ == "__main__":
                 tweet = li.find("p", class_ = "TweetTextSize")
                 if tweet:
                     tweet = tweet.get_text()
-                    ts = li.find("span", class_ = "_timestamp js-short-timestamp")
+                    ts = li.find("span", class_ = "_timestamp")
                     if ts:
                         ts = ts["data-time-ms"]
                 dbgp(("tweet:", tweet))
@@ -66,10 +71,12 @@ if __name__ == "__main__":
                     r = r + (li.find("a", class_ = "account-group")['href'],)
                     for a in replies.find_all('a'):
                         r = r + (a['href'],)
+                if not tweet or ts == -1:
+                    continue
                 activities.append((tweet, ts, r))
-            print(activities)
-            print(profile_elem)
-
+            dbgp(activities)
+            dbgp(profile_elem)
+            dtos[twitter_id] = (profile_elem, activities)
             if debug:
                 with open(twitter_id + ".html", "w", encoding = "utf-8") as html_file:
                     html_file.write(soup.prettify())
@@ -77,8 +84,27 @@ if __name__ == "__main__":
             print("Got status:", r.status_code)
             print(r)
 
-
-
+    if ui:
+        app = QApplication([])
+        window = QWidget()
+        layout = QVBoxLayout()
+        for twitter_id in twitter_ids:
+            # (url, stats, bio, location)
+            # [(tweet, timestamp, (author, reply1, reply2,...))]
+            profile_elem, activities = dtos[twitter_id]
+            profile_url, profile_stats, bio, location = profile_elem
+            profile_label = QLabel(profile_url + "\n" + str(location) + "\n" + bio + ",".join(profile_stats))
+            layout.addWidget(profile_label)
+            for (tweet, ts, replies) in activities:
+                dbgp((tweet, ts, replies))
+                tweet_text = ""
+                if replies:
+                    tweet_text += "@" + replies[0] + " replies to " + ",".join("@" + tid for tid in replies[1:])
+                tweet_text += "\n" + tweet + "...at " + datetime.datetime.fromtimestamp(float(ts)/1000).strftime("%Y-%m-%d %H:%M:%S")
+                layout.addWidget(QLabel(tweet_text))
+        window.setLayout(layout)
+        window.show()
+        app.exec_()
 
 
 
